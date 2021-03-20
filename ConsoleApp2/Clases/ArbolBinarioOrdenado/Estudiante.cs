98a025ec59256d09df678127ce37f24f52d7669b
Deyber Castillo <53123654+11dcastillo@users.noreply.github.com>
2021-03-19 22:24:17 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Clases/ArbolBinarioOrdenado/Estudiante.cs
@@ -0,0 +1,43 @@
+
+
+/**
+ *
+ * @author Ruldin
+ */
+public class Estudiante : Comparador {
+
+    public string descripcion;
+    public string nombre;
+
+    public bool igualQue(object q)
+    {
+        return true;
+    }
+
+    public bool mayorIgualQue(object q)
+    {
+        Estudiante p2 = (Estudiante)q;
+        return nombre.CompareTo(p2.nombre) >= 0;
+    }
+
+    public bool mayorQue(object q)
+    {
+        Estudiante p2 = (Estudiante)q;
+        return nombre.CompareTo(p2.nombre) > 0;
+
+    }
+
+    public bool menorIgualQue(object q)
+    {
+        Estudiante p2 = (Estudiante)q;
+        return nombre.CompareTo(p2.nombre) <= 0;
+    }
+
+    public bool menorQue(object op2) 
+    {
+
+        Estudiante p2 = (Estudiante)op2;
+        return nombre.CompareTo(p2.nombre) < 0;
+    }
+
+}
